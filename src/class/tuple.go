b5d13491813954247accf1e038b5e194cf03036e
Yufei Lin <yufei_lin_ulyssess@outlook.com>
2019-09-04 18:56:16 -0400

    Made two more functions

--- src/class/tuple.go
+++ src/class/tuple.go
@@ -4,20 +4,53 @@ import (
 	"fmt"
 )
 
-//Vertex Class
-type Vertex struct {
+//Tuple Class
+type Tuple struct {
 	X, Y, Z, W float64
 }
 
 //GetTuple tells whether the input of the vector is correct or not
-func (v Vertex) GetTuple() (x, y, z float64, typeOfTuple bool) {
+func (v Tuple) GetTuple() (x, y, z float64, typeOfTuple bool) {
 	if v.W == 1.0 {
 		return v.X, v.Y, v.Z, true
 	}
 	return v.X, v.Y, v.Z, false
 }
 
+//Point checks whether the tuple is a point
+func Point(x, y, z float64) Tuple {
+	v := Tuple{x, y, z, 1.0}
+	return v
+}
+
+//Vector checks whether the tuple is a vector
+func Vector(x, y, z float64) Tuple {
+	v := Tuple{x, y, z, 0}
+	return v
+}
+
+//Add adds two tuple together
+func (tone Tuple) Add(ttwo Tuple) Tuple {
+	var ans Tuple
+	ans.X = tone.X + ttwo.X
+	ans.Y = tone.Y + ttwo.Y
+	ans.Z = tone.Z + ttwo.Z
+	ans.W = tone.W + ttwo.W
+	return ans
+}
+
+//Subtract subtracts ttwo from tone
+func (tone Tuple) Subtract(ttwo Tuple) Tuple {
+	var ans Tuple
+	ans.X = tone.X - ttwo.X
+	ans.Y = tone.Y - ttwo.Y
+	ans.Z = tone.Z - ttwo.Z
+	ans.W = tone.W + ttwo.W
+	return ans
+}
+
 func main() {
-	v := Vertex{4.3, -4.2, 3.1, 1.0}
-	fmt.Println(v.GetTuple())
+	vone := Tuple{4.3, -4.2, 3.1, 1}
+	vtwo := Tuple{4.3, -4.2, 3.1, 1}
+	fmt.Println(vone.Add(vtwo))
 }
--- src/test/tuple_test.go
+++ src/test/tuple_test.go
@@ -5,16 +5,16 @@ import (
 	"testing"
 )
 
-//Test to see if the GetTuple function works for class tuple
+//TestTuple1 tests to see if the GetTuple function works for class tuple
 func TestTuple1(t *testing.T) {
 	tables := []struct {
-		v       class.Vertex
+		v       class.Tuple
 		x, y, z float64
 		point   bool
 	}{
-		{class.Vertex{4.3, -4.2, 3.1, 1.0}, 4.3, -4.2, 3.1, true},
-		{class.Vertex{4.5, -42, 310, 0.0}, 4.5, -42, 310, false},
-		{class.Vertex{4.5, -42, 310, 0.0}, 4.5, -42, 310, false},
+		{class.Tuple{4.3, -4.2, 3.1, 1.0}, 4.3, -4.2, 3.1, true},
+		{class.Tuple{4.5, -42, 310, 0.0}, 4.5, -42, 310, false},
+		{class.Tuple{4.5, -42, 310, 0.0}, 4.5, -42, 310, false},
 	}
 	for _, table := range tables {
 		x, y, z, typeOfTuple := table.v.GetTuple()
@@ -30,26 +30,51 @@ func TestTuple1(t *testing.T) {
 	}
 }
 
+//TestTuple2 tests to see if the function Point works as the way we want
 func TestTuple2(t *testing.T) {
 	tables := []struct {
-		v       class.Vertex
 		x, y, z float64
-		point   bool
+		ans     class.Tuple
 	}{
-		{class.Vertex{4.3, -4.2, 3.1, 1.0}, 4.3, -4.2, 3.1, true},
-		{class.Vertex{4.5, -42, 310, 0.0}, 4.5, -42, 310, false},
-		{class.Vertex{4.5, -42, 310, 0.0}, 4.5, -42, 310, false},
+		{4.3, -4.2, 3.1, class.Tuple{4.3, -4.2, 3.1, 1.0}},
 	}
 	for _, table := range tables {
-		x, y, z, typeOfTuple := table.v.GetTuple()
-		if x != table.x || y != table.y || z != table.z {
-			t.Errorf("Error Input")
-		} else {
-			if !typeOfTuple {
-				t.Errorf("Not a point")
-			} else {
-				t.Errorf("It's a point")
-			}
+		point := class.Point(table.x, table.y, table.z)
+		if point != table.ans {
+			t.Error("You are wrong")
+		}
+	}
+}
+
+//TestTuple3 tests to see if the function Vector works as the way we want
+func TestTuple3(t *testing.T) {
+	tables := []struct {
+		x, y, z float64
+		ans     class.Tuple
+	}{
+		{4.3, -4.2, 3.1, class.Tuple{4.3, -4.2, 3.1, 0}},
+	}
+	for _, table := range tables {
+		point := class.Vector(table.x, table.y, table.z)
+		if point != table.ans {
+			t.Error("You are wrong")
+		}
+	}
+}
+
+//TestTuple4 tests to see if the function Add works as the way we want
+func TestTuple4(t *testing.T) {
+	tables := []struct {
+		tone class.Tuple
+		ttwo class.Tuple
+		ans  class.Tuple
+	}{
+		{class.Tuple{4.3, -4.2, 3.1, 0}, class.Tuple{4.3, -4.2, 3.1, 1}, class.Tuple{8.6, -8.4, 6.2, 1}},
+	}
+	for _, table := range tables {
+		ans := table.tone.Add(table.ttwo)
+		if ans != table.ans {
+			t.Error("You are wrong")
 		}
 	}
 }
