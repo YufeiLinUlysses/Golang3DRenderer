c63d4d71f1b2f0787caf3546cb6d8152be342420
Yufei Lin <yufei_lin_ulyssess@outlook.com>
2019-09-04 11:57:27 -0400

    Make Change to ReadMe and add new tests

---
+++ src/test/tuple_test.go
@@ -0,0 +1,27 @@
+package test
+
+import (
+	"testing"
+	"class"
+)
+
+func TestTuple(t *testing.T) {
+	tables := []struct {
+		v       class.Vertex
+		x, y, z float64
+		point   bool
+	}{
+		{class.Vertex{4.3, -4.2, 3.1, 1.0}, 4.3, -4.2, 3.1, true},
+		{class.Vertex{4.5, -42, 310, 0.0}, 4.5, -42, 310, false},
+	}
+	for _, table := range tables {
+		x, y, z, typeOfTuple := table.v.GetTuple()
+		if x != table.x || y != table.y || z != table.z {
+			t.Errorf("Error Input")
+		} else {
+			if !typeOfTuple {
+				t.Errorf("Not a point")
+			}
+		}
+	}
+}
