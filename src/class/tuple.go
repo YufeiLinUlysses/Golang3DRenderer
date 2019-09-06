c1754f9ae277e3a9ee0d97d94f742029276de1d0
Yufei Lin <yufei_lin_ulyssess@outlook.com>
2019-09-06 13:56:35 -0400

    Still need to add cross product test

--- src/class/tuple.go
+++ src/class/tuple.go
@@ -2,6 +2,7 @@ package class
 
 import (
 	"fmt"
+	"math"
 )
 
 //Tuple Class
@@ -40,17 +41,98 @@ func (tone Tuple) Add(ttwo Tuple) Tuple {
 }
 
 //Subtract subtracts ttwo from tone
-func (tone Tuple) Subtract(ttwo Tuple) Tuple {
-	var ans Tuple
+func (tone Tuple) Subtract(ttwo Tuple) (ans Tuple, typeOfTuple bool) {
 	ans.X = tone.X - ttwo.X
 	ans.Y = tone.Y - ttwo.Y
 	ans.Z = tone.Z - ttwo.Z
-	ans.W = tone.W + ttwo.W
+	ans.W = tone.W - ttwo.W
+	if ans.W != 1 {
+		typeOfTuple = false
+	} else {
+		typeOfTuple = true
+	}
+	return ans, typeOfTuple
+}
+
+//Mutiply helps the tuple mutiplies a scalar
+func (t Tuple) Multiply(scalar float64) Tuple {
+	var ans Tuple
+	ans.X = t.X * scalar
+	ans.Y = t.Y * scalar
+	ans.Z = t.Z * scalar
+	ans.W = t.W * scalar
 	return ans
 }
 
+//Divides helps the tuple divide by a scalar
+func (t Tuple) Divide(scalar float64) Tuple {
+	var ans Tuple
+	if scalar != 0 {
+		ans.X = t.X / scalar
+		ans.Y = t.Y / scalar
+		ans.Z = t.Z / scalar
+		ans.W = t.W / scalar
+	} else {
+		fmt.Println("You are dividing by 0")
+		return ans
+	}
+	return ans
+}
+
+//Magnitude calculates the magnitude of a vector
+func (t Tuple) Magnitude() (mag float64, vecOrNot bool) {
+	if t.W != 1 {
+		mag = math.Sqrt(t.X*t.X + t.Y*t.Y + t.Z*t.Z)
+		return mag, true
+	} else {
+		mag = 0
+		return mag, false
+	}
+
+}
+
+//Normalize normalizes the vector by dividing each element with magnitude
+func (t Tuple) Normalize() (ans Tuple, normalized bool) {
+	mag, vecOrNot := t.Magnitude()
+	if vecOrNot && mag != 0 {
+		normalized = true
+		ans.X = t.X / mag
+		ans.Y = t.Y / mag
+		ans.Z = t.Z / mag
+		return ans, normalized
+	} else if vecOrNot && mag == 0 {
+		normalized = false
+		fmt.Println("A zero vector")
+		return ans, normalized
+	} else {
+		normalized = false
+		return ans, normalized
+	}
+}
+
+//DotProduct calculates the dot product of two vectors
+func (tone Tuple) DotProduct(ttwo Tuple) (ans float64, dotted bool) {
+	if tone.W != 1 && ttwo.W != 1 {
+		return tone.X*ttwo.X + tone.Y*ttwo.Y + tone.Z * ttwo.Z, true
+	} else {
+		return 0, false
+	}
+}
+
+//CrossProduct calculates the cross product of two vectors
+func (tone Tuple) CrossProduct(ttwo Tuple) (ans Tuple, crossed bool) {
+	if tone.W != 1 && ttwo.W != 1 {
+		ans.X = tone.Y*ttwo.Z - tone.Z*ttwo.Y
+		ans.Y = tone.Z*ttwo.X - tone.X*ttwo.Z
+		ans.Z = tone.X*ttwo.Y - tone.Y*ttwo.X
+		return ans, true
+	} else {
+		return ans, false
+	}
+}
+
 func main() {
-	vone := Tuple{4.3, -4.2, 3.1, 1}
-	vtwo := Tuple{4.3, -4.2, 3.1, 1}
-	fmt.Println(vone.Add(vtwo))
+	vone := Tuple{1, 2, 3, 0}
+	vtwo := Tuple{2, 3, 4, 0}
+	fmt.Println(vone.CrossProduct(vtwo))
 }
--- src/test/tuple_test.go
+++ src/test/tuple_test.go
@@ -78,3 +78,140 @@ func TestTuple4(t *testing.T) {
 		}
 	}
 }
+
+//TestTuple5 tests to see if the function Subtract works as the way we want
+//For now if we use a vector to subtract a point we will
+//get a vector unless otherwise instructed
+func TestTuple5(t *testing.T) {
+	tables := []struct {
+		tone        class.Tuple
+		ttwo        class.Tuple
+		ans         class.Tuple
+		typeOfTuple bool
+	}{
+		{class.Tuple{4.3, -4.2, 3.1, 1}, class.Tuple{4.3, -4.2, 3.1, 0}, class.Tuple{0, 0, 0, 1}, true},
+		{class.Tuple{4.3, -4.2, 3.1, 0}, class.Tuple{4.3, -4.2, 3.1, 0}, class.Tuple{0, 0, 0, 0}, false},
+		{class.Tuple{4.3, -4.2, 3.1, 1}, class.Tuple{4.3, -4.2, 3.1, 1}, class.Tuple{0, 0, 0, 0}, false},
+		{class.Tuple{4.3, -4.2, 3.1, 0}, class.Tuple{4.3, -4.2, 3.1, 1}, class.Tuple{0, 0, 0, -1}, false},
+		{class.Tuple{0, 0, 0, 0}, class.Tuple{4.3, -4.2, 3.1, 0}, class.Tuple{-4.3, 4.2, -3.1, 0}, false},
+	}
+	for _, table := range tables {
+		ans, typeOfTuple := table.tone.Subtract(table.ttwo)
+		if ans != table.ans || typeOfTuple != table.typeOfTuple {
+			t.Error("You are wrong")
+		}
+	}
+}
+
+//TestTuple6 tests to see if the function Multiply works as the way we want
+func TestTuple6(t *testing.T) {
+	tables := []struct {
+		tone class.Tuple
+		num  float64
+		ans  class.Tuple
+	}{
+		{class.Tuple{4.3, -4.2, 3.1, 1}, -1, class.Tuple{-4.3, 4.2, -3.1, -1}},
+		{class.Tuple{4.3, -4.2, 3.1, 0}, 2, class.Tuple{8.6, -8.4, 6.2, 0}},
+	}
+	for _, table := range tables {
+		ans := table.tone.Multiply(table.num)
+		if ans != table.ans {
+			t.Errorf("You are wrong, need %f,  %f, %f, %f, but get %f, %f, %f, %f", table.ans.X, table.ans.Y, table.ans.Z, table.ans.W, ans.X, ans.Y, ans.Z, ans.W)
+		}
+	}
+}
+
+//TestTuple7 tests to see if the function Divide works as the way we want
+func TestTuple7(t *testing.T) {
+	tables := []struct {
+		tone class.Tuple
+		num  float64
+		ans  class.Tuple
+	}{
+		{class.Tuple{4.3, -4.2, 3.1, 1}, -1, class.Tuple{-4.3, 4.2, -3.1, -1}},
+		{class.Tuple{4.3, -4.2, 3.1, 0}, 0, class.Tuple{0, 0, 0, 0}},
+	}
+	for _, table := range tables {
+		ans := table.tone.Divide(table.num)
+		if ans != table.ans {
+			t.Errorf("You are wrong, need %f,  %f, %f, %f, but get %f, %f, %f, %f", table.ans.X, table.ans.Y, table.ans.Z, table.ans.W, ans.X, ans.Y, ans.Z, ans.W)
+		}
+	}
+}
+
+//TestTuple8 tests to see if the function Magnitude works as the way we want
+func TestTuple8(t *testing.T) {
+	tables := []struct {
+		tone     class.Tuple
+		ans      float64
+		vecOrNot bool
+	}{
+		{class.Tuple{4.3, -4.2, 3.1, 1}, 0, false},
+		{class.Tuple{4.3, -4.2, 3.1, 0}, 6.763135367564367, true},
+	}
+	for _, table := range tables {
+		ans, vecOrNot := table.tone.Magnitude()
+		if ans != table.ans || vecOrNot != table.vecOrNot {
+			t.Errorf("You are wrong")
+		}
+	}
+}
+
+//TestTuple9 tests to see if the function Normalize works as the way we want
+func TestTuple9(t *testing.T) {
+	tables := []struct {
+		tone       class.Tuple
+		ans        class.Tuple
+		normalized bool
+	}{
+		{class.Tuple{4.3, -4.2, 3.1, 1}, class.Tuple{0, 0, 0, 0}, false},
+		{class.Tuple{4.3, -4.2, 3.1, 0}, class.Tuple{0.6357997831335106, -0.6210137416652894, 0.4583672855148565, 0}, true},
+		{class.Tuple{0, 0, 0, 0}, class.Tuple{0, 0, 0, 0}, false},
+	}
+	for _, table := range tables {
+		ans, normalized := table.tone.Normalize()
+		if ans != table.ans || normalized != table.normalized {
+			t.Errorf("You are wrong")
+		}
+	}
+}
+
+//TestTuple10 tests to see if the function Normalize works as the way we want
+//by checking whether the magnitude of the normalized  is 1
+func TestTuple10(t *testing.T) {
+	tables := []struct {
+		tone class.Tuple
+		mag  float64
+	}{
+		{class.Tuple{4.3, -4.2, 3.1, 0}, 1},
+	}
+	for _, table := range tables {
+		ans, normalized := table.tone.Normalize()
+		if normalized {
+			mag, _ := ans.Magnitude()
+			if !normalized || mag != 1 {
+				t.Errorf("You are wrong")
+			}
+		}
+	}
+}
+
+//TestTuple11 tests to see if the function DotProduct works as the way we want
+func TestTuple11(t *testing.T) {
+	tables := []struct {
+		tone   class.Tuple
+		ttwo   class.Tuple
+		ans    float64
+		dotted bool
+	}{
+		{class.Tuple{1, 2, 3, 0}, class.Tuple{2, 3, 4, 0}, 20, true},
+		{class.Tuple{1, 2, 3, 1}, class.Tuple{2, 3, 4, 0}, 0, false},
+		{class.Tuple{1, 2, 3, 1}, class.Tuple{2, 3, 4, 1}, 0, false},
+	}
+	for _, table := range tables {
+		ans, dotted := table.tone.DotProduct(table.ttwo)
+		if ans != table.ans || dotted != table.dotted {
+			t.Errorf("You are wrong, %f", ans)
+		}
+	}
+}
