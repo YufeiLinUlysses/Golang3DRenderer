b8382cc621ceb01c1886c2439c7006c5d23c0f0d
Lin <yufei_lin_ulyssess@outlook.com>
2019-09-09 19:43:30 -0400

    Left out something

--- src/class/canvas.go
+++ src/class/canvas.go
@@ -1,6 +1,8 @@
 package class
 
-import(
+import (
+	"fmt"
+	"os"
 	"strconv"
 )
 
@@ -10,14 +12,24 @@ type Canvas struct {
 	Canv          [][]Color
 }
 
-//Convert float to string
+//FloatToString converts float to string
 func FloatToString(num float64) string {
 	return strconv.FormatFloat(num, 'f', 6, 64)
 }
 
-//Convert int to string
+//IntToString converts int to string
 func IntToString(num int) string {
 	return strconv.FormatInt(int64(num), 10)
+}
+
+//ConvertToNum converts pixel number to float between 0 and 255
+func ConvertToNum(num float64) float64 {
+	if num >= 255 {
+		num = 255
+	} else if num <= 0 {
+		num = 0
+	}
+	return num
 }
 
 //NewCanvas establishes a new Canvas instance
@@ -52,8 +64,66 @@ func (c *Canvas) PixelAt(rw, cl int) Color {
 	return c.Canv[rw][cl]
 }
 
-//CanvasToPPM writes to PPM file
-func (c *Canvas) CanvasToPPM() string{
+//CanvasToString writes to PPM file
+func (c *Canvas) CanvasToString() string {
+	var ans string
 	PPMHeader := "P3\n" + IntToString(c.Width) + " " + IntToString(c.Height) + "\n" + "255\n"
+	ans += PPMHeader
 
+	for i := 0; i < c.Width; i++ {
+		var temp string
+		for j := 0; j < c.Height; j++ {
+			var blue string
+			red := FloatToString(ConvertToNum(c.Canv[i][j].R*255)) + " "
+			green := FloatToString(ConvertToNum(c.Canv[i][j].G*255)) + " "
+			if j == (c.Height - 1) {
+				blue = FloatToString(ConvertToNum(c.Canv[i][j].B*255)) + "\n"
+			} else {
+				blue = FloatToString(ConvertToNum(c.Canv[i][j].B*255)) + " "
+			}
+			if len(temp)+len(red) <= 70 {
+				temp += red
+			} else {
+				ans += temp + "\n"
+				temp = red
+			}
+			if len(temp)+len(green) <= 70 {
+				temp += green
+			} else {
+				ans += temp + "\n"
+				temp = green
+			}
+			if len(temp)+len(blue) <= 70 {
+				temp += blue
+			} else {
+				ans += temp + "\n"
+				temp = blue
+			}
+		}
+		ans += temp
+	}
+
+	return ans
+}
+
+//CanvasToPPM saves the canvas string to ppm file
+func CanvasToPPM(ppm string, title string) {
+	fileTitle := title + ".ppm"
+	f, err := os.Create(fileTitle)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	l, err := f.WriteString(ppm)
+	if err != nil {
+		fmt.Println(err)
+		f.Close()
+		return
+	}
+	fmt.Println(l, "bytes written successfully")
+	err = f.Close()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
