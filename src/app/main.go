b8cac6f2adebd14895d7b92d1d5d8afd06bbe06e
Yufei Lin <yufei_lin_ulyssess@outlook.com>
2019-09-12 10:24:23 -0400

    Still need more work on canvas test

--- src/app/main.go
+++ src/app/main.go
@@ -1,28 +1,29 @@
 package main
 
 import (
-	"class"
 	"fmt"
-	"io/ioutil"
-	"os"
+	"path/filepath"
 )
 
+//"class"
+
 func main() {
-	canv := class.NewCanvas(5, 3)
-	cone := class.NewColor(1.5, 0, 0)
-	ctwo := class.NewColor(0, 0.5, 0)
-	cthree := class.NewColor(-0.5, 0, 1)
-	canv.WritePixel(0, 0, cone)
-	canv.WritePixel(2, 1, ctwo)
-	canv.WritePixel(4, 2, cthree)
-	PPM := canv.CanvasToString()
-	canv.CanvasToPPM("hahaha")
-	fmt.Println(PPM)
-	dat, _ := os.Open("test.ppm")
-	b, _ := ioutil.ReadAll(dat)
-	ans1 := string(b)
-	dat1, _ := os.Open("hahaha.ppm")
-	b1, _ := ioutil.ReadAll(dat1)
-	ans2 := string(b1)
-	fmt.Println(ans1 == ans2)
+	fmt.Println(filepath.Dir("../test/testFiles/originalFile/"))
+	// canv := class.NewCanvas(5, 3)
+	// cone := class.NewColor(1.5, 0, 0)
+	// ctwo := class.NewColor(0, 0.5, 0)
+	// cthree := class.NewColor(-0.5, 0, 1)
+	// canv.WritePixel(0, 0, cone)
+	// canv.WritePixel(2, 1, ctwo)
+	// canv.WritePixel(4, 2, cthree)
+	// PPM := canv.CanvasToString()
+	// canv.CanvasToPPM("hahaha")
+	// fmt.Println(PPM)
+	// dat, _ := os.Open("test.ppm")
+	// b, _ := ioutil.ReadAll(dat)
+	// ans1 := string(b)
+	// dat1, _ := os.Open("hahaha.ppm")
+	// b1, _ := ioutil.ReadAll(dat1)
+	// ans2 := string(b1)
+	// fmt.Println(ans1 == ans2)
 }
--- src/test/canvas_test.go
+++ src/test/canvas_test.go
@@ -2,6 +2,7 @@ package test
 
 import (
 	"class"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"testing"
@@ -61,6 +62,31 @@ func TestCanvas3(t *testing.T) {
 	b, _ := ioutil.ReadAll(dat)
 	ans1 := string(b)
 	dat1, _ := os.Open("hahaha.ppm")
+	b1, _ := ioutil.ReadAll(dat1)
+	ans2 := string(b1)
+	if ans1 != ans2 {
+		t.Errorf("You are wrong")
+	}
+}
+
+//TestCanvas4 tests to see if we change everything
+//will CanvasToPPm still work for class Canvas
+func TestCanvas4(t *testing.T) {
+	canv := class.NewCanvas(10, 2)
+	for i := 0; i < canv.Height; i++ {
+		for j := 0; j < canv.Width; j++ {
+			canv.Canv[i][j] = *class.NewColor(1, 0.8, 0.6)
+		}
+	}
+	canv.CanvasToPPM("hhh")
+	files, err:= ioutil.ReadDir("../testFiles/originalFile")
+	fmt.Println(files)
+	fmt.Println(err)
+	dat, err := os.Open("testFiles/originalFile/test2.ppm")
+	b, _ := ioutil.ReadAll(dat)
+	ans1 := string(b)
+	fmt.Println("ans1:\n" + ans1)
+	dat1, _ := os.Open("hhh.ppm")
 	b1, _ := ioutil.ReadAll(dat1)
 	ans2 := string(b1)
 	if ans1 != ans2 {
