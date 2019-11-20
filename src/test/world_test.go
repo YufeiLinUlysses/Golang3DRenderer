ec9990c273e65e1cb99fbf2740da4c806367e6d2
Yufei Lin <yufei_lin_ulyssess@outlook.com>
2019-11-20 10:30:17 -0500

    Added some test

--- src/test/world_test.go
+++ src/test/world_test.go
@@ -6,8 +6,25 @@ import (
 	"testing"
 )
 
-//TestWorld1 tests to see if the ColorAt function works for feature World
+//TestWorld1 tests to see if the NewWorld function works for feature World
 func TestWorld1(t *testing.T) {
+	tables := []struct {
+		ansL1, ansL2 int
+	}{
+		{0, 0},
+	}
+	for _, table := range tables {
+		testLight := make([]feature.Light, 0)
+		testShpae := make([]interface{}, 0)
+		w := feature.NewWorld(testLight, testShpae)
+		if len(w.Objects) != table.ansL1 || len(w.Lights) != table.ansL2 {
+			t.Errorf("Error")
+		}
+	}
+}
+
+//TestWorld2 tests to see if the ColorAt function works for feature World
+func TestWorld2(t *testing.T) {
 	tables := []struct {
 		r    *feature.Ray
 		ansC *feature.Color
@@ -17,10 +34,10 @@ func TestWorld1(t *testing.T) {
 	}
 	for _, table := range tables {
 		w := feature.DefaultWorld()
-		c := w.ColorAt(table.r,5)
+		c := w.ColorAt(table.r, 5)
 		errorAllowance := 0.00001
 		if math.Abs(c.R-table.ansC.R) > errorAllowance || math.Abs(c.G-table.ansC.G) > errorAllowance || math.Abs(c.B-table.ansC.B) > errorAllowance {
-			t.Errorf("Error Input %v, %v", c,table.ansC)
+			t.Errorf("Error Input %v, %v", c, table.ansC)
 		}
 	}
 }
